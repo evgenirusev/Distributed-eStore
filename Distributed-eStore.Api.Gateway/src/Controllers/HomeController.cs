267bd644e755337a776528999102b15ddfd48018
evgenirusev <evgeni.n.rusev@gmail.com>
2020-11-28 13:09:12 +0200

    Send request from gateway to product service to test

--- Distributed-eStore.Api.Gateway/src/Controllers/HomeController.cs
+++ Distributed-eStore.Api.Gateway/src/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace Distributed_eStore.Api.Gateway.Controllers
 {
@@ -7,5 +9,16 @@ namespace Distributed_eStore.Api.Gateway.Controllers
     {
         [HttpGet]
         public IActionResult Index() => Ok("Distributed-eStore Home Page");
+
+        [HttpGet]
+        public async Task<IActionResult> CallProductService()
+        {
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync("http://localhost:5001");
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            return Ok(responseBody);
+        }
     }
 }
