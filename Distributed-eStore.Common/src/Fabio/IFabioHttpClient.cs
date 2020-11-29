972a108dbdf1b2f0b4fe938b7397ac1907cd2e81
evgenirusev <evgeni.n.rusev@gmail.com>
2020-11-30 01:51:09 +0200

    Fix packages, remove excess repo

--- Distributed-eStore.Common/src/Fabio/IFabioHttpClient.cs
+++
@@ -1,9 +0,0 @@
-using System.Threading.Tasks;
-
-namespace DistributedEStore.Common.Fabio
-{
-    public interface IFabioHttpClient
-    {
-        Task<T> GetAsync<T>(string requestUri);
-    }
-}
--- Distributed-eStore.Common/src/IServiceId.cs
+++
@@ -1,7 +0,0 @@
-namespace DistributedEStore.Common
-{
-    public interface IServiceId
-    {
-         string Id { get; }
-    }
-}
