ba1d78a6736d6f19849ba6e31c29a877e0466dbb
evgenirusev <evgeni.n.rusev@gmail.com>
2020-12-08 20:19:20 +0200

    add return value for http get request in products microservice

--- Distributed-eStore.Services.Product/src/Controllers/ProductsController.cs
+++ Distributed-eStore.Services.Product/src/Controllers/ProductsController.cs
@@ -18,33 +18,7 @@ namespace DistributedEStore.Services.Product.Controllers
         [HttpGet]
         public async Task<ActionResult<PagedResult<ProductDto>>> Get([FromQuery] BrowseProducts query)
         {
-            Post("https://testtest.free.beeceptor.com", "Hi from ProductsController");
-            var x = this.QueryAsync(query);
-
-            return Ok("hi from ProductsController - Get endpoint");
-        }
-
-        public string Post(string uri, string data, string contentType = "application/json", string method = "POST")
-        {
-            byte[] dataBytes = Encoding.UTF8.GetBytes(data);
-
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
-            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
-            request.ContentLength = dataBytes.Length;
-            request.ContentType = contentType;
-            request.Method = method;
-
-            using (Stream requestBody = request.GetRequestStream())
-            {
-                requestBody.Write(dataBytes, 0, dataBytes.Length);
-            }
-
-            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
-            using (Stream stream = response.GetResponseStream())
-            using (StreamReader reader = new StreamReader(stream))
-            {
-                return reader.ReadToEnd();
-            }
+            return Ok(this.QueryAsync(query));
         }
     }
 }
