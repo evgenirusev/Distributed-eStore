0505c1d861d7581becde8996c8fe0a076ff68204
evgenirusev <evgeni.n.rusev@gmail.com>
2020-12-11 01:15:37 +0200

    Configure services

--- Distributed-eStore.Services.Identity/src/Startup.cs
+++ Distributed-eStore.Services.Identity/src/Startup.cs
@@ -1,16 +1,20 @@
+using Autofac;
+using Consul;
+using DistributedEStore.Common;
+using DistributedEStore.Common.Authentication;
+using DistributedEStore.Common.Consul;
+using DistributedEStore.Common.Dispatchers;
+using DistributedEStore.Common.Mongo;
+using DistributedEStore.Common.Mvc;
+using DistributedEStore.Common.RabbitMq;
+using DistributedEStore.Services.Identity.Domain;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Reflection;
 
 namespace Distributed_eStore.Services.Identity
 {
@@ -23,37 +27,63 @@ namespace Distributed_eStore.Services.Identity
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
+            services.AddInitializers(typeof(IMongoDbInitializer));
+            services.AddOpenTracing();
+            services.AddCors(options =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Distributed_eStore.Services.Identity", Version = "v1" });
+                options.AddPolicy("CorsPolicy", cors =>
+                        cors.AllowAnyOrigin()
+                            .AllowAnyMethod()
+                            .AllowAnyHeader()
+                            .AllowCredentials());
             });
-        }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
+            using (var serviceProvider = services.BuildServiceProvider())
             {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Distributed_eStore.Services.Identity v1"));
+                var configuration = serviceProvider.GetService<IConfiguration>();
+                services.Configure<AppOptions>(configuration.GetSection("app"));
             }
 
-            app.UseHttpsRedirection();
+            services.AddSingleton<DistributedEStore.Common.Mvc.IServiceId, ServiceId>();
+            services.AddTransient<IStartupInitializer, StartupInitializer>();
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
-            app.UseRouting();
+            services.AddConsul();
+            services.AddControllers();
+        }
 
-            app.UseAuthorization();
+        public void ConfigureContainer(ContainerBuilder builder)
+        {
+            builder.RegisterAssemblyTypes(Assembly.GetEntryAssembly())
+                    .AsImplementedInterfaces();
+            builder.AddMongo();
+            builder.AddMongoRepository<RefreshToken>("RefreshTokens");
+            builder.AddMongoRepository<User>("Users");
+            builder.AddRabbitMq();
+            builder.AddDispatchers();
+            builder.RegisterType<PasswordHasher<User>>().As<IPasswordHasher<User>>();
+        }
 
-            app.UseEndpoints(endpoints =>
+        public void Configure(IApplicationBuilder app, IStartupInitializer startupInitializer,
+            IConsulClient client, IHostApplicationLifetime applicationLifetime)
+        {
+            app.UseCors("CorsPolicy");
+            app.UseAllForwardedHeaders();
+            app.UseErrorHandler();
+            app.UseAuthentication();
+            app.UseAccessTokenValidator();
+            app.UseServiceId();
+            app.UseRabbitMq();
+
+            var consulServiceId = app.UseConsul();
+            applicationLifetime.ApplicationStopped.Register(() =>
             {
-                endpoints.MapControllers();
+                client.Agent.ServiceDeregister(consulServiceId);
             });
+
+            startupInitializer.InitializeAsync();
         }
     }
 }
