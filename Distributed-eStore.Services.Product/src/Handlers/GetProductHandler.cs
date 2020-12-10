5b35ef5d39ed301792872ef431dd9fb5338bf57f
evgenirusev <evgeni.n.rusev@gmail.com>
2020-12-10 21:16:25 +0200

    add get product by id

---
+++ Distributed-eStore.Services.Product/src/Handlers/GetProductHandler.cs
@@ -0,0 +1,29 @@
+﻿using DistributedEStore.Common.Handlers;
+using DistributedEStore.Services.Products.Dto;
+using DistributedEStore.Services.Products.Queries;
+using DistributedEStore.Services.Products.Repositories;
+using System.Threading.Tasks;
+
+namespace DistributedEStore.Services.Products.Handlers
+{
+    public sealed class GetProductHandler : IQueryHandler<GetProduct, ProductDto>
+    {
+        private readonly IProductsRepository _productsRepository;
+
+        public GetProductHandler(IProductsRepository productsRepository)
+            => _productsRepository = productsRepository;
+
+        public async Task<ProductDto> HandleAsync(GetProduct query)
+        {
+            var product = await _productsRepository.GetAsync(query.Id);
+
+            return product == null ? null : new ProductDto
+            {
+                Id = product.Id,
+                Name = product.Name,
+                Description = product.Description,
+                Price = product.Price
+            };
+        }
+    }
+}
