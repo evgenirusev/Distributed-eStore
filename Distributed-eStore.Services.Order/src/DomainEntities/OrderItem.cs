e0915b5be24edccf25d02256e03f7ed5c0e187a9
evgenirusev <evgeni.n.rusev@gmail.com>
2021-01-10 19:08:02 +0200

    Order Item domain entity

---
+++ Distributed-eStore.Services.Order/src/DomainEntities/OrderItem.cs
@@ -0,0 +1,44 @@
+﻿using DistributedEStore.Common.Types;
+using System;
+
+namespace DistributedEStore.Services.Products.DomainEntities
+{
+    public class OrderItem : BaseEntity
+    {
+        public Guid ProductId { get; set; }
+        public int Quantity { get; set; }
+        public string Size { get; set; }
+
+        public OrderItem(Guid id, string productId, int quantity, string size)
+            : base(id)
+        {
+            SetProductId(id);
+            SetQuantity(quantity);
+            SetSize(size);
+        }
+
+        public void SetProductId(Guid productId)
+        {
+            ProductId = productId;
+            SetUpdatedDate();
+        }
+
+        public void SetQuantity(int quantity)
+        {
+            if (quantity <= 0)
+            {
+                throw new DistributedEStoreException("invalid_product_quantity",
+                    "Quantity cannot be zero or negative.");
+            }
+
+            Quantity = quantity;
+            SetUpdatedDate();
+        }
+
+        public void SetSize(string size)
+        {
+            Size = size.Trim().ToLowerInvariant();
+            SetUpdatedDate();
+        }
+    }
+}
