811b46497c5079a3f1e31e9916bfa7b349e9432d
evgenirusev <evgeni.n.rusev@gmail.com>
2021-01-18 18:13:47 +0200

    UI home page UI work

--- Distributed-eStore.UI/src/ClientApp/src/components/HomePage/HomePage.js
+++ Distributed-eStore.UI/src/ClientApp/src/components/HomePage/HomePage.js
@@ -5,7 +5,10 @@ var React = require("react");
 require("./HomePage.css");
 var HomePage = function () {
     return React.createElement("div", { className: "home-page row" },
-        React.createElement("img", { className: "products-image", src: "https://i.ibb.co/8xntqgb/Hero-Banner-Template-Reusables-Sale-8c7da083-67c2-4043-b6af-49ed3be027e8-2000x.jpg" }));
+        React.createElement("div", { className: "image-container" },
+            React.createElement("img", { className: "image-container__image", src: "https://i.ibb.co/8xntqgb/Hero-Banner-Template-Reusables-Sale-8c7da083-67c2-4043-b6af-49ed3be027e8-2000x.jpg" }),
+            React.createElement("div", { className: "home-page__text-container" },
+                React.createElement("h1", { className: "home-page__text" }, "Just Launched Summer Products"))));
 };
 exports.HomePage = HomePage;
 //# sourceMappingURL=HomePage.js.map
--- Distributed-eStore.UI/src/ClientApp/src/components/HomePage/HomePage.tsx
+++ Distributed-eStore.UI/src/ClientApp/src/components/HomePage/HomePage.tsx
@@ -3,6 +3,11 @@ import "./HomePage.css";
 
 export const HomePage: React.FC = () => {
     return <div className="home-page row">
-        <img className="products-image" src="https://i.ibb.co/8xntqgb/Hero-Banner-Template-Reusables-Sale-8c7da083-67c2-4043-b6af-49ed3be027e8-2000x.jpg"></img>
+        <div className="image-container">
+            <img className="image-container__image" src="https://i.ibb.co/8xntqgb/Hero-Banner-Template-Reusables-Sale-8c7da083-67c2-4043-b6af-49ed3be027e8-2000x.jpg"></img>
+            <div className="home-page__text-container">
+                <h1 className="home-page__text">Just Launched Summer Products</h1>
+            </div>
+        </div>
     </div>
 };
