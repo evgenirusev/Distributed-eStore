523ea1e23da88817fc713405c828845e141eff59
evgenirusev <evgeni.n.rusev@gmail.com>
2021-01-18 23:32:45 +0200

    shopping cart

--- Distributed-eStore.UI/src/ClientApp/src/components/cart/cartWidget/CartWidget.js
+++ Distributed-eStore.UI/src/ClientApp/src/components/cart/cartWidget/CartWidget.js
@@ -2,11 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.CartWidget = void 0;
 var React = require("react");
+require("./CartWidget.css");
 var CartWidget = function (_a) {
     var numberOfItems = _a.numberOfItems;
-    return React.createElement("div", { className: "cart-badge badge-icons pull-right" },
+    return React.createElement("div", { className: "cart cart-badge badge-icons pull-right" },
         React.createElement("i", { className: "fa fa-shopping-cart" }),
-        React.createElement("span", { className: "badge badge-danger rounded-x" }, numberOfItems),
+        React.createElement("span", { className: "badge badge-primary rounded-x cart-number-of-items" }, numberOfItems),
         React.createElement("div", { className: "badge-open" }));
 };
 exports.CartWidget = CartWidget;
--- Distributed-eStore.UI/src/ClientApp/src/components/cart/cartWidget/CartWidget.tsx
+++ Distributed-eStore.UI/src/ClientApp/src/components/cart/cartWidget/CartWidget.tsx
@@ -1,4 +1,5 @@
 ﻿import * as React from 'react';
+import "./CartWidget.css";
 
 type CartWidgetProps = {
     numberOfItems: number;
@@ -7,10 +8,10 @@ type CartWidgetProps = {
 export const CartWidget: React.FC<CartWidgetProps> = ({
     numberOfItems
 }) => {
-    return <div className="cart-badge badge-icons pull-right">
-        <i className="fa fa-shopping-cart"></i>
-        <span className="badge badge-danger rounded-x">{numberOfItems}</span>
-        <div className="badge-open">
+    return <div className="cart cart-badge badge-icons pull-right">
+            <i className="fa fa-shopping-cart"></i>
+            <span className="badge badge-primary rounded-x cart-number-of-items">{numberOfItems}</span>
+            <div className="badge-open">
         </div>
     </div>
 };
